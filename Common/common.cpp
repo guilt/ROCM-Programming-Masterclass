e3d7ee1d7a4968a7551148312b671532d7bcba17
Karthik Kumar Viswanathan <karthikkumar@gmail.com>
2023-02-19 23:28:28 +0000

    Add 15-SumOfArrays and 15-SumOfThreeArrays. Add common.[h cpp]. Add hipCommon.[hip hiph]

---
+++ Common/common.cpp
@@ -0,0 +1,408 @@
+#include "common.h"
+
+#include <cfloat>
+#include <cstring>
+#include <fstream>
+
+#include <time.h>
+#ifdef _WIN32
+#include <sys/utime.h>
+#else
+#include <sys/time.h>
+#endif
+
+//Print Time using Host Clock
+void printTimeUsingHostClock(clock_t start, clock_t end, const char *msg)
+{
+	printf("%s time : %4.6f \n", msg, 
+		(double)((double)(end - start) / CLOCKS_PER_SEC));
+}
+
+// Compare Floats
+static bool areEqual(float a, float b, float epsilon = FLT_EPSILON) {
+    return (fabs(a - b) <= epsilon * std::max(1.0f, std::max(a, b)));
+}
+
+//Compare Results
+void compareResults(int gpuResult, int cpuResult)
+{
+	if (gpuResult == cpuResult)
+	{
+		printf("GPU and CPU results are same \n");
+		return;
+	}
+
+	printf("GPU and CPU results are different \n");
+	printf("GPU result : %d, CPU result : %d \n",
+		gpuResult, cpuResult);
+}
+
+//Compare Results
+void compareResults(float gpuResult, float cpuResult)
+{
+	if (areEqual(gpuResult, cpuResult))
+	{
+		printf("GPU and CPU results are same \n");
+		return;
+	}
+
+	printf("GPU and CPU results are different \n");
+	printf("GPU result : %.2f, CPU result : %.2f \n",
+		gpuResult, cpuResult);
+}
+
+//Simple Initialization
+void initializeArray(int * input, const int arraySize,
+	INIT_PARAM PARAM, int x)
+{
+	if (PARAM == INIT_ZERO)
+	{
+		for (int i = 0; i < arraySize; i++)
+		{
+			input[i] = 0;
+		}
+	}
+	else if (PARAM == INIT_ONE)
+	{
+		for (int i = 0; i < arraySize; i++)
+		{
+			input[i] = 1;
+		}
+	}
+	else if (PARAM == INIT_ONE_TO_TEN)
+	{
+		for (int i = 0; i < arraySize; i++)
+		{
+			input[i] = i % 10;
+		}
+	}
+	else if (PARAM == INIT_RANDOM)
+	{
+		srand(time(NULL));
+		for (int i = 0; i < arraySize; i++)
+		{
+			input[i] = (int)(rand() % (x+1));
+		}
+	}
+	else if (PARAM == INIT_FOR_SPARSE_MATRIX)
+	{
+		int value;
+		srand(time(NULL));
+		for (int i = 0; i < arraySize; i++)
+		{
+			value = rand() % 25;
+			if (value < 5)
+			{
+				input[i] = value;
+			}
+			else
+			{
+				input[i] = 0;
+			}
+		}
+	}
+}
+
+//Simple Initialization
+void initializeArray(float * input, const int arraySize,
+	INIT_PARAM PARAM, int x)
+{
+	if (PARAM == INIT_ZERO)
+	{
+		for (int i = 0; i < arraySize; i++)
+		{
+			input[i] = 0;
+		}
+	}
+	else if (PARAM == INIT_ONE)
+	{
+		for (int i = 0; i < arraySize; i++)
+		{
+			input[i] = 1;
+		}
+	}
+	else if (PARAM == INIT_ONE_TO_TEN)
+	{
+		for (int i = 0; i < arraySize; i++)
+		{
+			input[i] = i % 10;
+		}
+	}
+	else if (PARAM == INIT_RANDOM)
+	{
+		srand(time(NULL));
+		for (int i = 0; i < arraySize; i++)
+		{
+			input[i] = (rand() % (x+1));
+		}
+	}
+	else if (PARAM == INIT_FOR_SPARSE_MATRIX)
+	{
+		int value;
+		srand(time(NULL));
+		for (int i = 0; i < arraySize; i++)
+		{
+			value = rand() % 25;
+			if (value < 5)
+			{
+				input[i] = value;
+			}
+			else
+			{
+				input[i] = 0;
+			}
+		}
+	}
+}
+
+//Print Array
+void printArray(int * input, const int arraySize)
+{
+	for (int i = 0; i < arraySize; i++)
+	{
+		if (!(i == (arraySize - 1)))
+		{
+			printf("%d,", input[i]);
+		}
+		else
+		{
+			printf("%d \n", input[i]);
+		}
+	}
+}
+
+//Print Array
+void printArray(float * input, const int arraySize)
+{
+	for (int i = 0; i < arraySize; i++)
+	{
+		if (!(i == (arraySize - 1)))
+		{
+			printf("%.2f,", input[i]);
+		}
+		else
+		{
+			printf("%.2f \n", input[i]);
+		}
+	}
+}
+
+//Compare Two Arrays
+void compareArrays(int * a, int * b, const int arraySize)
+{
+	for (int  i = 0; i < arraySize; i++)
+	{
+		if (a[i] != b[i])
+		{
+			printf("Arrays are different \n");
+			printf("%d - %d vs. %d \n", i, a[i], b[i]);
+			return;
+		}
+	}
+	printf("Arrays are same \n");
+}
+
+//Compare Two Arrays
+void compareArrays(float * a, float * b, const int arraySize)
+{
+	for (int i = 0; i < arraySize; i++)
+	{
+		if (!areEqual(a[i], b[i]))
+		{
+			printf("Arrays are different \n");
+			printf("%d - %.2f vs. %.2f \n", i, a[i], b[i]);
+			return;
+		}
+	}
+	printf("Arrays are same \n");
+}
+
+//Sum Two Arrays with CPU
+void sumArraysCpu(int * a, int * b, int * result, const int arraySize)
+{
+	for (int i = 0; i < arraySize; i++)
+	{
+		result[i] = a[i] + b[i];
+	}
+}
+
+//Sum Two Arrays with CPU
+void sumArraysCpu(float * a, float * b, float * result, const int arraySize)
+{
+	for (int i = 0; i < arraySize; i++)
+	{
+		result[i] = a[i] + b[i];
+	}
+}
+
+//Sum Three Arrays with CPU
+void sumArraysCpu(int * a, int * b, int * c, int * result, const int arraySize)
+{
+	for (int i = 0; i < arraySize; i++)
+	{
+		result[i] = a[i] + b[i] + c[i];
+	}
+}
+
+//Sum Three Arrays with CPU
+void sumArraysCpu(float * a, float * b, float * c, float * result, const int arraySize)
+{
+	for (int i = 0; i < arraySize; i++)
+	{
+		result[i] = a[i] + b[i] + c[i];
+	}
+}
+
+//Array Reduction with CPU
+int reductionArrayCpu(int * input, const int arraySize)
+{
+	int sum = 0;
+	for (int i = 0; i < arraySize; i++)
+	{
+		sum += input[i];
+	}
+	return sum;
+}
+
+//Array Reduction with CPU
+float reductionArrayCpu(float * input, const int arraySize)
+{
+	float sum = 0;
+	for (int i = 0; i < arraySize; i++)
+	{
+		sum += input[i];
+	}
+	return sum;
+}
+
+//Print Arrays to a File
+void printArraysToAFile(int * a, const int arraySize, const char * fileName)
+{
+	std::ofstream file(fileName);
+
+	if (file.is_open())
+	{
+		for (int i = 0; i < arraySize; i++) {
+			file << i << " - " << a[i] << "\n";
+		}
+		file.close();
+	}
+}
+
+//Print Arrays to a File
+void printArraysToAFile(float * a, const int arraySize, const char * fileName)
+{
+	std::ofstream file(fileName);
+
+	if (file.is_open())
+	{
+		for (int i = 0; i < arraySize; i++) {
+			file << i << " - " << a[i] << "\n";
+		}
+		file.close();
+	}
+}
+
+//Print Arrays to a File Side By Side
+void printArraysToAFileSideBySide(int * a, int * b, const int arraySize, const char * fileName)
+{
+	std::ofstream file(fileName);
+
+	if (file.is_open())
+	{
+		for (int i = 0; i < arraySize; i++) {
+			file << i << " - " << a[i] << " - " << b[i] << "\n";
+		}
+		file.close();
+	}
+}
+
+//Print Arrays To a File Side by Side
+void printArraysToAFileSideBySide(float * a, float * b, const int arraySize, const char * fileName)
+{
+	std::ofstream file(fileName);
+
+	if (file.is_open())
+	{
+		for (int i = 0; i < arraySize; i++) {
+			file << i << " - " <<a[i] << " - " << b[i] << "\n";
+		}
+		file.close();
+	}
+}
+
+//Initialize Matrix
+int* initializeMatrix(const int nx, const int ny, INIT_PARAM PARAM)
+{
+	int matSize = ny * nx;
+	int matrixByteSize = sizeof(int)*matSize;
+
+	int *mat = (int*)malloc(matrixByteSize);
+
+	initializeArray(mat, matSize, PARAM);
+	return mat;
+}
+
+//Initialize Matrix
+float* initializeFloatMatrix(const int nx, const int ny, INIT_PARAM PARAM)
+{
+	int matSize = ny * nx;
+	int matrixByteSize = sizeof(float)*matSize;
+
+	float *mat = (float*)malloc(matrixByteSize);
+
+	initializeArray(mat, matSize, PARAM);
+	return mat;
+}
+
+//Print Matrix
+void printMatrix(int * matrix, const int nx, const int ny)
+{
+	for (int iy = 0; iy < ny; iy++)
+	{
+		for (int ix = 0; ix < nx; ix++)
+		{
+			printf("%d ",matrix[nx * iy + ix]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+//Print Matrix
+void printMatrix(float * matrix, const int nx, const int ny)
+{
+	for (int iy = 0; iy < ny; iy++)
+	{
+		for (int ix = 0; ix < nx; ix++)
+		{
+			printf("%.2f ", matrix[nx * iy + ix]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+//Matrix Transpose with CPU
+void matrixTransposeCpu(int * mat, int * transpose, const int nx, const int ny)
+{
+	for (int  iy = 0; iy < ny; iy++)
+	{
+		for (int  ix = 0; ix < nx; ix++)
+		{
+			transpose[ix * ny + iy] = mat[iy * nx + ix];
+		}
+	}
+}
+
+//Matrix Transpose with CPU
+void matrixTransposeCpu(float * mat, float * transpose, const int nx, const int ny)
+{
+	for (int  iy = 0; iy < ny; iy++)
+	{
+		for (int  ix = 0; ix < nx; ix++)
+		{
+			transpose[ix * ny + iy] = mat[iy * nx + ix];
+		}
+	}
+}
---
+++ Common/common.h
@@ -0,0 +1,110 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define HANDLE_NULL( a ) do { \
+				if (a == NULL) { \
+					printf( "Host memory failed in %s at line %d\n", \
+							__FILE__, __LINE__ );  \
+					exit( EXIT_FAILURE ); \
+				} \
+			} while(0);
+
+//Array Initialization
+enum INIT_PARAM{
+	INIT_ZERO,
+	INIT_ONE,
+	INIT_ONE_TO_TEN,
+	INIT_RANDOM,
+	INIT_FOR_SPARSE_MATRIX
+};
+
+//Launch Dummy Kernel
+inline void launchDummmyKernel() {}
+
+//Print Time using Host Clock
+void printTimeUsingHostClock(clock_t start, clock_t end, const char *msg = "GPU Kernel Execution");
+
+//Compare Results
+void compareResults(int gpuResult, int cpuResult);
+
+//Compare Results
+void compareResults(float gpuResult, float cpuResult);
+
+//Simple Initialization
+void initializeArray(int * input, const int arraySize,
+	INIT_PARAM PARAM = INIT_ONE_TO_TEN, int x = 0xFF);
+
+//Simple Initialization
+void initializeArray(float * input, const int arraySize,
+	INIT_PARAM PARAM = INIT_ONE_TO_TEN, int x = 0xFF);
+
+//Print Array
+void printArray(int * input, const int arraySize);
+
+//Print Array
+void printArray(float * input, const int arraySize);
+
+//Print Array with Message
+#define printMsgArray(msg, array, arraySize) do { printf("%s: ", msg); printArray(array, arraySize); } while(0);
+
+//Compare Two arrays
+void compareArrays(int * a, int * b, const int arraySize);
+
+//Compare Two Arrays
+void compareArrays(float * a, float * b, const int arraySize);
+
+//Sum Two Arrays with CPU
+void sumArraysCpu(int * a, int * b, int * result, const int arraySize);
+
+//Sum Two Arrays with CPU
+void sumArraysCpu(float * a, float * b, float * result, const int arraySize);
+
+//Sum Three Arrays with CPU
+void sumArraysCpu(int * a, int * b, int * c, int * result, const int arraySize);
+
+//Sum Three Arrays with CPU
+void sumArraysCpu(float * a, float * b, float * c, float * result, const int arraySize);
+
+//Array Reduction with CPU
+int reductionArrayCpu(int * input, const int arraySize);
+
+//Array Reduction with CPU
+float reductionArrayCpu(float * input, const int arraySize);
+
+//Print Arrays to a File
+void printArraysToAFile(int * a, const int arraySize, const char * fileName);
+
+//Print Arrays to a File
+void printArraysToAFile(float * a, const int arraySize, const char * fileName);
+
+//Print Arrays to a File Side By Side
+void printArraysToAFileSideBySide(int * a, int * b, const int arraySize, const char * fileName);
+
+//Print Arrays to a File Side By Side
+void printArraysToAFileSideBySide(float * a, float * b, const int arraySize, const char * fileName);
+
+//Initialize Matrix
+int* initializeMatrix(const int nx, const int ny, INIT_PARAM PARAM=INIT_FOR_SPARSE_MATRIX);
+
+//Initialize Matrix
+float* initializeFloatMatrix(const int nx, const int ny, INIT_PARAM PARAM=INIT_FOR_SPARSE_MATRIX);
+
+//Print Matrix
+void printMatrix(int * matrix, const int nx, const int ny);
+
+//Print Matrix
+void printMatrix(float * matrix, const int nx, const int ny);
+
+//Print Matrix with Message
+#define printMsgMatrix(msg, mat, nx, ny) do { printf("%s: \n", msg); printMatrix(mat, nx, ny); } while(0);
+
+//Matrix Transpose with CPU
+void matrixTransposeCpu(int * mat, int * transpose, const int nx, const int ny);
+
+//Matrix Transpose with CPU
+void matrixTransposeCpu(float * mat, float * transpose, const int nx, const int ny);
+
+#endif // !COMMON_H
